d206e0a3cce52d3b20346216cd2696b5270e6c65
Schrödinger's Cat <18612775+SchrodingersCpp@users.noreply.github.com>
2023-01-05 01:26:28 +0200

    Corrected stripping integer part zeros error

--- num_string_funcs.h
+++ num_string_funcs.h
@@ -10,15 +10,17 @@
 // Converts a number to string.
 template <typename T>
 std::string Number2String(const T num, const int prec=10) {
-  if (num == 0) { return std::string(1, '0'); }
   std::ostringstream oss_num;
   oss_num << std::fixed << std::setprecision(prec) << num;
   std::string str_num{ oss_num.str() };
   // Pointer to last char.
   const char* ptr{ str_num.data() + str_num.size() - 1 };
-  // Strips trailing zeros and decimal point (if it is last char).
-  while (*ptr == '0') { --ptr; }
-  if (*ptr == '.') { --ptr; }
+  // Remove decimal zeros only.
+  if (str_num.find('.') != std::string::npos) {
+    // Strips trailing zeros and decimal point (if it is last char).
+    while (*ptr == '0') { --ptr; }
+    if (*ptr == '.') { --ptr; }
+  }
   str_num.resize(ptr - str_num.data() + 1);
   return str_num;
 }
