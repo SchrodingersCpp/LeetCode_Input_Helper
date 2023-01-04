bf95e65fcac4acd4ec3a9e500c8bd3ef401e99e8
Schrödinger's Cat <18612775+SchrodingersCpp@users.noreply.github.com>
2023-01-04 11:05:30 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,13 +1,14 @@
 #include <string>
 
+#include "treenode_funcs.h"
 #include "treenode.h"
 #include "txt_to_treenode.h"
 
 int main() {
   using T = int;
   const std::string txt_list{ "[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]" };
-  
   TreeNode<T>* root{ Txt2TreeNode<T>(txt_list) };
+  PrintTreeNode(root);
 
   return 0;
 }
