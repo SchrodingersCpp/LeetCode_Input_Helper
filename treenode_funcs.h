3dc5fa6268631a34c0969f385fed41faf2e71cbd
Schrödinger's Cat <18612775+SchrodingersCpp@users.noreply.github.com>
2023-01-08 15:56:49 +0200

    Corrected tree layout error

--- treenode_funcs.h
+++ treenode_funcs.h
@@ -97,6 +97,28 @@ void GrowInitBranches(std::vector<std::vector<std::string>>& vec_lvl) {
   }
 }
 
+// Auxiliary formatting data.
+void FormatData(
+    std::vector<std::size_t>& dist_to_l_elem,
+    std::vector<std::size_t>& dist_btw_elems,
+    std::vector<std::size_t>& brch_width,
+    const std::vector<std::vector<std::string>>& vec_lvl,
+    const std::size_t min_dist) {
+  const std::size_t n_lvls{ vec_lvl.size() };
+  const std::size_t elem_len{ vec_lvl[0][0].size() };
+  std::vector<std::size_t> pair_width(n_lvls);  // Width of a pair of elems.
+  pair_width.back() = elem_len;
+  dist_to_l_elem.back() = 0;
+  dist_btw_elems.back() = min_dist;
+  brch_width.back() = 0;
+  for (std::size_t i{ n_lvls - 2 }; i < n_lvls; --i) {
+    pair_width[i] = 2 * pair_width[i+1] + min_dist;
+    dist_to_l_elem[i] = (pair_width[i] - elem_len) / 2;
+    dist_btw_elems[i] = pair_width[i] - elem_len + min_dist;
+    brch_width[i] = dist_to_l_elem[i] - (dist_to_l_elem[i+1] + elem_len / 2);
+  }
+}
+
 // Builds a vector with formatted the TreeNode levels.
 std::vector<std::string> FormatTreeNodeLvls(
     const std::vector<std::vector<std::string>>& vec_lvl,
@@ -106,31 +128,30 @@ std::vector<std::string> FormatTreeNodeLvls(
   const std::size_t n_lvls{ vec_lvl.size() };
   const std::size_t elem_len{ vec_lvl[0][0].size() };
   std::vector<std::string> vec_fmt_lvl(n_lvls);  // Formatted lines.
-  std::vector<std::size_t> pair_width(n_lvls);  // Width of a pair of elems.
-  std::vector<std::size_t> dist_btw_elems(n_lvls);  // Spacing btw neighbors.
   std::vector<std::size_t> dist_to_l_elem(n_lvls);  // Spacing to first elem.
-  std::vector<std::size_t> n_branch_len(n_lvls);  // Length of an elem branch.
-  pair_width.back() = elem_len;
-  dist_btw_elems.back() = min_dist;
-  dist_to_l_elem.back() = 0;
-  n_branch_len.back() = 0;
-  for (std::size_t i{ n_lvls - 2 }; i <= n_lvls; --i) {
-    pair_width[i] = 2 * pair_width[i+1] + min_dist;
-    dist_btw_elems[i] = pair_width[i] - elem_len + min_dist;
-    dist_to_l_elem[i] = (pair_width[i] - elem_len) / 2;
-    n_branch_len[i] = dist_to_l_elem[i] - (dist_to_l_elem[i+1] + elem_len / 2);
-  }
+  std::vector<std::size_t> dist_btw_elems(n_lvls);  // Spacing btw neighbors.
+  std::vector<std::size_t> brch_width(n_lvls);  // Length of an elem branch.
+  FormatData(
+      dist_to_l_elem, dist_btw_elems, brch_width,
+      vec_lvl, min_dist);
   for (std::size_t i_lvl{}; i_lvl < n_lvls; ++i_lvl) {
+    const std::size_t n_elems{ vec_lvl[i_lvl].size() };
+    const std::string spacing(
+        dist_btw_elems[i_lvl] - 2 * brch_width[i_lvl], space_char);
     std::string& fmt_lvl{ vec_fmt_lvl[i_lvl] };
-    std::size_t n_left_spaces{ dist_to_l_elem[i_lvl] - n_branch_len[i_lvl] };
-    const std::string branch(n_branch_len[i_lvl], brch_char);
-    fmt_lvl = std::string(n_left_spaces, space_char) + branch;
-    const std::string inner_spacing(
-        dist_btw_elems[i_lvl] - 2 * n_branch_len[i_lvl], space_char);
-    for (std::size_t i_elem{}; i_elem < (vec_lvl[i_lvl].size() - 1); ++i_elem) {
-      fmt_lvl += vec_lvl[i_lvl][i_elem] + branch + inner_spacing + branch;
+    const std::size_t n_left_spaces{ dist_to_l_elem[i_lvl] - brch_width[i_lvl] };
+    fmt_lvl = std::string(n_left_spaces, space_char);
+    for (std::size_t i_elem{}; i_elem < n_elems; ++i_elem) {
+      char branch_char{ brch_char };
+      if (IsCenteredStrEmpty(vec_lvl[i_lvl][i_elem])) {
+        branch_char = space_char;
+      }
+      const std::string branch(brch_width[i_lvl], branch_char);
+      fmt_lvl += branch + vec_lvl[i_lvl][i_elem] + branch;
+      if (i_elem != (n_elems - 1)) {
+        fmt_lvl += spacing;
+      }
     }
-    fmt_lvl += vec_lvl[i_lvl].back() + branch;
   }
   return vec_fmt_lvl;
 }
