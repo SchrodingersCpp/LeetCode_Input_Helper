068c28e8ddfe0c522cf4d20e343db22ea1e9bbdb
Schrödinger's Cat <18612775+SchrodingersCpp@users.noreply.github.com>
2023-01-21 10:34:44 +0200

    Implemented conversion function

--- txt_to_vecvec.h
+++ txt_to_vecvec.h
@@ -1,10 +1,12 @@
 #ifndef TXT_TO_VECVEC_H
 #define TXT_TO_VECVEC_H
 
+#include <cstddef>
 #include <string>
 #include <vector>
 
 #include "matrix_string.h"
+#include "txt_to_treenode.h"
 
 // Converts a text vector-vector input to a vector-vector object.
 template <typename T>
@@ -13,7 +15,13 @@ std::vector<std::vector<T>> Txt2VecVec(
     const char row_end=']', const char col_delim=',') {
   const std::vector<std::vector<std::string>> txt_vecvec{
       Txt2VecVecStr(txt_mtx, row_end, col_delim) };
-  
+  const std::size_t n_rows{ txt_vecvec.size() };
+  std::vector<std::vector<T>> vecvec(n_rows);
+  for (std::size_t i{}; i < n_rows; ++i) {
+    auto [vec_elems, vec_valid] = VecValues<T>(txt_vecvec[i]);
+    vecvec[i] = vec_elems;
+  }
+  return vecvec;
 }
 
 #endif  // TXT_TO_VECVEC_H
